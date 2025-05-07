41a0a75d7937e0f5189049a657d825112d18c7d5
Iván Agüero Acosta <126924582+1van2004@users.noreply.github.com>
2025-05-07 11:24:57 -0600

    Update NavBar.jsx
    Los cambios no se acoplaron por completo al ultimo cambio, entonces lo actualice

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,11 +2,11 @@ import { Link } from "@tanstack/react-router"
 
 const NavBar = () => {
     return(
-        <nav className="bg-gray-800 text-white px-6 py-4 flex justify-between items-center shadow">
+       <nav className="bg-gradient-to-r from-green-400 via-cyan-400 to-blue-500 text-white px-6 py-4 flex justify-between items-center shadow">
             <Link to="/" className="text-lg front-medium hover:text-gray-200">Inicio</Link>
             <Link to="/works" className="text-lg front-medium hover:text-gray-200">Tareas</Link>
             <Link to="/proveedores" className="text-lg front-medium hover:text-gray-200">Proveedores</Link>
-            <Link to="/Inventario" className="text-lg front-medium hover:text-gray-300">Inventario</Link>
+            <Link to="/Inventario" className="text-lg front-medium hover:text-gray-200">Inventario</Link>
         </nav>
     )
 }
