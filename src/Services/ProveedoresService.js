6c7479ebb1308e3fc79ee26241ed8020da1012d3
MrNano08 <imrnano08@gmial.com>
2025-05-26 14:03:04 -0600

    Se cambió de jsonbin a una api en localhost

--- src/Services/ProveedoresService.js
+++ src/Services/ProveedoresService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_MASTER_KEY_Proveedores;
-const BASE_URL = import.meta.env.VITE_API_URL_Proveedores;
+const BASE_URL = "http://localhost:5067/api/Proveedores";
 const headers = {
   'Content-Type': 'application/json',
   'X-Master-Key': API_KEY,
@@ -9,18 +9,34 @@ const headers = {
 
 export const obtenerProveedores = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}`, { headers });
-    return response.data.record || [];
+    const response = await axios.get(BASE_URL, { headers });
+    return response.data || [];
   } catch (error) {
     console.error('Error al obtener proveedores:', error);
     return [];
   }
 };
 
-export const guardarProveedores = async (nuevosProveedores) => {
+export const crearProveedor = async (proveedor) => {
   try {
-    await axios.put(BASE_URL, nuevosProveedores, { headers });
+    await axios.post(BASE_URL, proveedor, { headers });
   } catch (error) {
-    console.error('Error al guardar proveedores:', error);
+    console.error('Error al crear proveedor:', error);
+  }
+};
+
+export const editarProveedor = async (proveedor) => {
+  try {
+    await axios.put(`${BASE_URL}/${proveedor.id}`, proveedor, { headers });
+  } catch (error) {
+    console.error('Error al editar proveedor:', error);
+  }
+};
+
+export const eliminarProveedor = async (id) => {
+  try {
+    await axios.delete(`${BASE_URL}/${id}`, { headers });
+  } catch (error) {
+    console.error('Error al eliminar proveedor:', error);
   }
 };
